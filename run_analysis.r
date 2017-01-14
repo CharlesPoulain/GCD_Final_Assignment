041f3fdca29701de17f22b164f252b0f22ac8d0e
CharlesPoulain <charlespoulaindo@yahoo.fr>
2017-01-15 00:53:30 +0100

    added mergedData function

--- run_analysis.r
+++ run_analysis.r
@@ -1,5 +1,4 @@
 main <- function(){
-    
     # ipak function: install and load multiple R packages.
     # check to see if packages are installed. Install them if they are not, then load them into the R session.
     
@@ -21,7 +20,6 @@ main <- function(){
     if(!dir.exists(data_path)){
         loadData()
     }
-    
     dt<-mergeData(data_path)
 }
 
@@ -35,7 +33,24 @@ loadData <-function(){
 }
 
 mergeData <-function(dir_path){
+    train_path<-paste(dir_path,"train",sep="/")
     
+    subject_train<-read.table(paste(train_path,"subject_train.txt",sep="/"))
+    activity_train<-read.table(paste(train_path,"y_train.txt",sep="/"))
+    measures_train<-read.table(paste(train_path,"X_train.txt",sep="/"))
+    train_data<-cbind(measures_train,subject_train,activity_train)
+    
+    test_path<-paste(dir_path,"test",sep="/")
+    
+    subject_test<-read.table(paste(test_path,"subject_test.txt",sep="/"))
+    activity_test<-read.table(paste(test_path,"y_test.txt",sep="/"))
+    measures_test<-read.table(paste(test_path,"X_test.txt",sep="/"))
+    test_data<-cbind(measures_test,subject_test,activity_test)
+    
+    merged_data<-rbind(train_data,test_data)
+    features_path<-paste(dir_path,"features.txt",sep="/")
+    names(merged_data)<-c(as.character(read.table(features_path)[,2]),"subject","activity")
+    return(merged_data)
 }
 
 extractFeat<-function(dir_path){
@@ -53,3 +68,4 @@ labelDataSet<-function(dir_path){
 writeData<-function(dir_path){
     
 }
+
